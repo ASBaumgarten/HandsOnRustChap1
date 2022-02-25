324501c9a813ae17bcc584fec53f076d8225b6e5
Alexander Baumgarten <alexander.s.baumgarten@gmail.com>
2022-02-25 11:08:59 -0500

    Added comments

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io::stdin;
+
+fn what_is_your_name()->String {
+    let mut your_name = String::new(); // declare variable with 'let' and make it mutable with 'mut'
+    stdin()
+        .read_line(&mut your_name) // take standard input, real_line to store it in 'your_name'
+        .expect("Failed to readline");
+    your_name // return 'your_name'
+}
+fn main() {
+    println!("What is your name?"); // print line to standard output
+    let name = what_is_your_name(); // declare 'name' and set its value equal to output of 'what_is_your_name'
+    println!("Hello, {}", name); // print line to output
+}
