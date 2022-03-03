a3860e6c69be18a9926d816a574edf2c461df142
Alexander Baumgarten <alexander.s.baumgarten@gmail.com>
2022-03-03 12:36:18 -0500

    First Struct

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,24 @@ fn what_is_your_name() -> String {
         .trim() // trim off hidden characters such as '\n'
         .to_lowercase() // convert to all lowercase
 }
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor{
+    fn new(name: &str, greeting:&str) -> Self{
+        Self{
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+    fn greet_visitor(&self){
+        println!("{}",self.greeting);
+    }
+}
+
 fn main() {
     println!("What is your name?"); // print line to standard output
     let name = what_is_your_name(); // declare 'name' and set its value equal to output of 'what_is_your_name'
